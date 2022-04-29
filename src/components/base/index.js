0481c383d9b2d515998536953936a43897a5c431
freezeburger <renaud.dubuis@decryptage.net>
2022-04-29 15:12:30 +0200

    useMediaQuery

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,3 +1,5 @@
+export {default as useMediaQuery } from '@mui/material/useMediaQuery';
+
 export { default as BaseButton } from './BaseButton/BaseButton';
 export { default as BaseHeader } from './BaseHeader/BaseHeader';
 export { default as BaseFooter } from './BaseFooter/BaseFooter';
