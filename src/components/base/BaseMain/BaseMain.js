e8b43e8e78af6bbf38be87944fc56b900377cb37
freezeburger <renaud.dubuis@decryptage.net>
2022-04-29 15:53:32 +0200

    Main Styled

--- src/components/base/BaseMain/BaseMain.js
+++ src/components/base/BaseMain/BaseMain.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './BaseMain.css';
+import styled from '@emotion/styled';
+
+const Main = styled.main`
+  background-color: #eee;
+  color:#222;
+  width:100vw;
+  height:80vh;
+  display:flex;
+  margin-top:5vh;
+  padding:20px;
+  padding-top:70px;
+  box-sizing:border-box;
+`;
 
 const BaseMain = ({children:content}) => (
-
-  
-  <>
+  <Main>
     {content}
-  </>
+  </Main>
 );
 
 BaseMain.propTypes = {};
