0fee86d54e670ad3ab7db575c184b8b4475b7fc1
freezeburger <renaud.dubuis@decryptage.net>
2022-04-29 17:08:40 +0200

    View MEssages

--- src/components/views/ViewMessages/ViewMessages.js
+++ src/components/views/ViewMessages/ViewMessages.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import LayoutDefault from '../../layouts';
+import { FeatureMessages } from '../../features';
 
 const ViewMessages = () => (
-  <div>
-    ViewMessages Component
-  </div>
+  <LayoutDefault>
+
+    <LayoutDefault.Header>
+      MESSAGES
+    </LayoutDefault.Header>
+
+    <LayoutDefault.Main>
+      <FeatureMessages></FeatureMessages>
+    </LayoutDefault.Main>
+
+    <LayoutDefault.Footer>
+      Brought to you by @AllTech !
+    </LayoutDefault.Footer>
+
+  </LayoutDefault>
 );
 
 ViewMessages.propTypes = {};
