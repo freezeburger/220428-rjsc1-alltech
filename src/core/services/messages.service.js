85fe34b57b12ca19c4256eb6981f11dcbc5d764c
freezeburger <renaud.dubuis@decryptage.net>
2022-05-02 15:14:08 +0200

    Meesage Service

---
+++ src/core/services/messages.service.js
@@ -0,0 +1,31 @@
+
+import { create } from "@mui/material/styles/createTransitions";
+import { ApiClient } from "../utils/api-client";
+
+
+const messages = [];
+
+export const MessagesService = {
+
+    init(){
+        this.load();
+    },
+
+    load(){
+        ApiClient.read('messages').then(res => {
+            messages.push(...res);
+        });
+    },
+
+    refresh(msg){
+        messages.length = 0;
+        this.load();
+    },
+
+    create(msg){
+        ApiClient.create('messages', msg).then(res => {
+            messages.push(res);
+        });
+    }
+
+}
