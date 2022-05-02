902d3e242bd9fd9e3eca2c99a17d7090cfcd0911
freezeburger <renaud.dubuis@decryptage.net>
2022-05-02 14:27:31 +0200

    Data Structure

--- src/components/base/BaseButton/BaseButton.test.js
+++ src/components/base/BaseButton/BaseButton.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import BaseButton from './BaseButton';
 
-describe('<BaseButton />', () => {
+fdescribe('<BaseButton />', () => {
   test('it should mount', () => {
     render(<BaseButton />);
     
