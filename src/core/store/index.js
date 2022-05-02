52c38a6291303ff70735107120d85785f060f5f9
freezeburger <renaud.dubuis@decryptage.net>
2022-05-02 12:38:32 +0200

    Redux Devtools

--- src/core/store/index.js
+++ src/core/store/index.js
@@ -1,9 +1,14 @@
 
 import { applyMiddleware, legacy_createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { actionStreamer } from "./middlewares/action-streamer.middleware";
 import { mainReducer } from "./reducers";
 
 
-const store = legacy_createStore(mainReducer, applyMiddleware(actionStreamer));
+const store = legacy_createStore(mainReducer,
+    composeWithDevTools(
+        applyMiddleware(actionStreamer)
+    )
+);
 
 export default store;
